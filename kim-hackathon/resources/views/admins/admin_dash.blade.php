f934a97ec09f2ef31aa421ad0ab45f0101de2393
hayyaoe <hayya947@gmail.com>
2023-09-22 23:15:18 +0700

    Added some view

--- kim-hackathon/resources/views/admins/admin_dash.blade.php
+++ kim-hackathon/resources/views/admins/admin_dash.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Admin's Dash</title>
 </head>
 <body>
-    <h1>Home view</h1>
+    <h1>Admin Pannel</h1>
 </body>
 </html>
--- kim-hackathon/resources/views/components/header.blade.php
+++ kim-hackathon/resources/views/components/header.blade.php
@@ -7,6 +7,6 @@
     <title>Document/<title>
 </head>
 <body>
-    
+    <h1>ini page index ya bang</h1>
 </body>
 </html>
