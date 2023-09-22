54df1efd78870ca8022d97409e5690f6bfdea3c4
hayyaoe <hayya947@gmail.com>
2023-09-23 03:45:16 +0700

    updated model and migrations

--- kim-hackathon/app/Models/Accounting.php
+++ kim-hackathon/app/Models/Accounting.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Accounting extends Model
 {
     use HasFactory;
+
+    public function admin(){
+        return $this->belongsTo(Admin::Class);
+    }
 }
